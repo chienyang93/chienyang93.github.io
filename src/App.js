e8fcf69395d9767d134e3638c2f07847c3c6cd50
chienyang93 <chienyang93@gmail.com>
2022-05-26 19:59:26 +0800

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,23 +1,50 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState, useEffect } from 'react';
+
+function getWindowDimensions() {
+  const { innerWidth: width, innerHeight: height } = window;
+  return {
+    width,
+    height
+  };
+}
+function useWindowDimensions() {
+  const [windowDimensions, setWindowDimensions] = useState(getWindowDimensions());
+  useEffect(() => {
+    function handleResize() {
+      setWindowDimensions(getWindowDimensions());
+    }
+    window.addEventListener('resize', handleResize);
+    return () => window.removeEventListener('resize', handleResize);
+  }, []);
+  return windowDimensions;
+}
+
+let padding = 5
 
 function App() {
+  const { height, width } = useWindowDimensions();
+  let box = { width: (width - padding * 3) / 2, height: (height - padding * 3) / 2 }
+  const [teamA, setTeamA] = useState(0);
+  const [teamB, setTeamB] = useState(0);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={{ width: width - padding * 2, height: height - padding * 2, padding: padding, background: 'gray', display: 'flex', justifyContent: 'space-between' }}>
+      <div style={{ width: box.width, display: 'flex', alignItems: 'center', justifyContent: 'center' }}>
+        <p style={{ fontSize: width * 0.4, color: 'white' }}>{teamA}</p>
+      </div>
+      <div style={{ width: box.width, display: 'flex', alignItems: 'center', justifyContent: 'center' }}>
+        <p style={{ fontSize: width * 0.4, color: 'white' }}>{teamB}</p>
+      </div>
+
+      <div onClick={() => { setTeamA(teamA + 1) }} style={{ position: 'absolute', top: padding, left: padding, width: box.width, height: box.height, background: 'black', opacity: 0.1 }}>
+      </div>
+      <div onClick={() => { teamA > 0 && setTeamA(teamA - 1) }} style={{ position: 'absolute', bottom: padding, left: padding, width: box.width, height: box.height, background: 'black', opacity: 0.1 }}>
+      </div>
+      <div onClick={() => { setTeamB(teamB + 1) }} style={{ position: 'absolute', top: padding, right: padding, width: box.width, height: box.height, background: 'black', opacity: 0.1 }}>
+      </div>
+      <div onClick={() => { teamB > 0 && setTeamB(teamB - 1) }} style={{ position: 'absolute', bottom: padding, right: padding, width: box.width, height: box.height, background: 'black', opacity: 0.1 }}>
+      </div>
+
     </div>
   );
 }
